83631c69576cbf526b33e62c344ed621d523ef90
Mahdi_Murshed <123mahdi12345@gmail.com>
2021-05-31 21:50:48 -0700

    provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screen/product_detail_screen.dart';
-import 'package:shop_app/widgets/product_item.dart';
 import 'screen/products_overview_screen.dart';
+import 'package:provider/provider.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.greenAccent,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (context) =>Products() ,
+          child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          accentColor: Colors.greenAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverViewScreen(),
+        routes: {
+          ProductDetailScreen.routeName:(ctx)=>ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverViewScreen(),
-      routes: {
-        ProductDetailScreen.routeName:(ctx)=>ProductDetailScreen(),
-      },
     );
   }
 }
