2f94717901f4d036c26f2a5e6448d6b8f2f5d373
Mahdi_Murshed <123mahdi12345@gmail.com>
2021-06-03 01:31:34 -0700

    cart begin

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screen/product_detail_screen.dart';
 import 'screen/products_overview_screen.dart';
 import 'package:provider/provider.dart';
-
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) =>Products() ,
-          child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'MyShop',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          accentColor: Colors.greenAccent,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverViewScreen(),
-        routes: {
-          ProductDetailScreen.routeName:(ctx)=>ProductDetailScreen(),
-        },
-      ),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => Products(),
+          ),
+          ChangeNotifierProvider(create: (context) => Cart()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+            accentColor: Colors.greenAccent,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverViewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          },
+        ));
   }
 }
-
-
---
+++ lib/providers/cart.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+  CartItem({this.id, this.title, this.quantity, this.price});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String id, double price, String title) {
+    if (_items.containsKey(id)) {
+      _items.update(id, (value) => CartItem(quantity: value.quantity + 1));
+    } else {
+      _items.putIfAbsent(
+          id,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
+    }
+  }
+}
